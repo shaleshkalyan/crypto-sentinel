be6757dd735c45706fc24d0e564a7f16c90e45af
Shalesh Kalyan <shaleshkalyan123@gmail.com>
2026-01-17 09:38:17 +0530

    Dev : Server and express setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from './app';
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`service running on port ${PORT}`);
+});
